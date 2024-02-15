86adf585686275a7a30857438ce7e87bab77245d
young jin <scn07074@naver.com>
2024-02-15 18:07:01 +0900

    first commit

---
+++ src/main/java/com/green/Finemove/FinemovieApplication.java
@@ -0,0 +1,13 @@
+package com.green.Finemove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinemovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinemovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/Finemove/FinemovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.Finemove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinemovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
