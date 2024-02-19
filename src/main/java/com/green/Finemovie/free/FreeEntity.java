60a028f2dad18e9804d5d19db69089fff10fdd43
Younghun Cho <jyh03172460@gmail.com>
2024-02-19 17:54:20 +0900

    24/02/19 행복 자유게시판 작업중

--- src/main/java/com/green/Finemovie/free/FreeEntity.java
+++ src/main/java/com/green/Finemovie/free/FreeEntity.java
@@ -1,5 +1,42 @@
 package com.green.Finemovie.free;
 
-public class FreeEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@SequenceGenerator(name = "gen_seq_free", sequenceName = "seq_free", initialValue = 1, allocationSize = 1)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "FreeBoards")
+public class FreeEntity {
+	
+	@Id
+	@GeneratedValue(generator = "gen_seq_free", strategy = GenerationType.SEQUENCE)
+	private long freeNo;
+	
+	@Column(nullable = false)
+	private String writer;
+	
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String content;
+	
 }
